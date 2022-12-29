350b457beec6da4bbb9edacf8eab1dfd2092e6df
mikobeako <111254258+mikobeako@users.noreply.github.com>
2022-12-29 15:31:50 -0800

    Update Misc.py

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -16,7 +16,14 @@ class Misc(commands.Cog):
         embed = discord.Embed()
         embed.set_image(url=f"attachment://{random}.jpg")
         await ctx.send(file=image, embed=embed)
+    
+    @commands.command()
+    async def joinczvr(self, ctx):
+        await ctx.send("```To join the CZVR FIR you will need to first be a member of VATCAN and have your S1 Rating.\n\nYou can request a transfer by following the steps here: https://czvr.ca/join. \n\nOnce accepted you will receive a email from no-reply@vatcan.ca with further instructions before you are placed on the waitlist. \n*NOTE: I highly recomend whitelisting any @vatcan.ca or @czvr.ca Emails in your spam filter*\n\n**... I mean SQUAWK.**```")
 
+    @commands.command()
+    async def link(self, ctx):
+        await ctx.send("```To link your Discord account and have your roles and name synced with the CZVR website goto https://czvr.ca and log in with your vatsim account, then goto your dashboard on the website and click 'link discord' then follow the steps.\nOnce youve done this it can take upto 24hrs for the bot to give you your roles, or you can update them manually with ~updateroles\n\n**... I mean SQUAWK.**```")
 
 async def setup(client):
     await client.add_cog(Misc(client))
